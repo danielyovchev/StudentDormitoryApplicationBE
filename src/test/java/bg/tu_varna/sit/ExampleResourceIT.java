63447623d7145fa55891c3e68f13971deb538606
danielyovchev <daniel.yovchev@gmail.com>
2024-03-21 15:57:44 +0200

    Modules separation

--- src/test/java/bg/tu_varna/sit/ExampleResourceIT.java
+++
@@ -1,8 +0,0 @@
-package bg.tu_varna.sit;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-class ExampleResourceIT extends ExampleResourceTest {
-    // Execute the same tests but in packaged mode.
-}
