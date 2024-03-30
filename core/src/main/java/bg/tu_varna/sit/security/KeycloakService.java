4a6c99a10428f9cb8e3fb72632754a3d4bf1e72b
danielyovchev <daniel.yovchev@gmail.com>
2024-03-30 22:45:11 +0200

    Initial keycloak security config and test

---
+++ core/src/main/java/bg/tu_varna/sit/security/KeycloakService.java
@@ -0,0 +1,8 @@
+package bg.tu_varna.sit.security;
+
+import jakarta.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+public class KeycloakService {
+
+}
--- rest/src/main/java/bg/tu_varna/sit/UserResource.java
+++ rest/src/main/java/bg/tu_varna/sit/UserResource.java
@@ -1,14 +1,25 @@
 package bg.tu_varna.sit;
 
+import io.quarkus.security.Authenticated;
+import jakarta.annotation.security.PermitAll;
+import jakarta.annotation.security.RolesAllowed;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.core.Response;
 
 @Path("/api/users")
+@Authenticated
 public class UserResource {
     @GET
     @Path("/example")
+    @PermitAll
     public Response exampleEndpoint(){
+        return Response.ok("Good").build();
+    }
+    @GET
+    @Path("/example/admin")
+    @RolesAllowed("admin")
+    public Response exampleEndpointAutg(){
         return Response.ok("Good").build();
     }
 }
