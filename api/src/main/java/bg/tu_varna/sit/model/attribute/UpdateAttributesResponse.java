a23b5a90c8495d2f6bf69c9d30a12781757c0ea1
danielyovchev <daniel.yovchev@gmail.com>
2024-06-08 17:56:37 +0300

    services to update and get rules and attributes

--- api/src/main/java/bg/tu_varna/sit/model/attribute/UpdateAttributesResponse.java
+++ api/src/main/java/bg/tu_varna/sit/model/attribute/UpdateAttributesResponse.java
@@ -1,10 +1,7 @@
 package bg.tu_varna.sit.model.attribute;
 
 import bg.tu_varna.sit.base.OperationResponse;
-import bg.tu_varna.sit.model.dto.AttributeDTO;
 import lombok.*;
-
-import java.util.List;
 
 @Getter
 @Setter(AccessLevel.PRIVATE)
--- core/src/main/java/bg/tu_varna/sit/services/rules/UpdateRulesService.java
+++ core/src/main/java/bg/tu_varna/sit/services/rules/UpdateRulesService.java
@@ -1,7 +1,6 @@
 package bg.tu_varna.sit.services.rules;
 
 import bg.tu_varna.sit.base.Error;
-import bg.tu_varna.sit.base.OperationProcessor;
 import bg.tu_varna.sit.entity.RuleEntity;
 import bg.tu_varna.sit.error.InternalError;
 import bg.tu_varna.sit.model.dto.RuleDTO;
