50e188a2c191322ba1439a0db55719620a7c80ad
danielyovchev <daniel.yovchev@gmail.com>
2024-06-26 17:26:41 +0300

    changes

--- api/src/main/java/bg/tu_varna/sit/model/application/SaveApplicationRequest.java
+++ api/src/main/java/bg/tu_varna/sit/model/application/SaveApplicationRequest.java
@@ -1,7 +1,6 @@
 package bg.tu_varna.sit.model.application;
 
 import bg.tu_varna.sit.base.OperationInput;
-import bg.tu_varna.sit.model.dto.ApplicationDTO;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
--- rest/src/main/java/bg/tu_varna/sit/cors/CorsFilter.java
+++ rest/src/main/java/bg/tu_varna/sit/cors/CorsFilter.java
@@ -6,12 +6,10 @@ import jakarta.ws.rs.container.ContainerResponseContext;
 import jakarta.ws.rs.container.ContainerResponseFilter;
 import jakarta.ws.rs.ext.Provider;
 
-import java.io.IOException;
-
 @Provider
 public class CorsFilter implements ContainerResponseFilter {
     @Override
-    public void filter(ContainerRequestContext containerRequestContext, ContainerResponseContext containerResponseContext) throws IOException {
+    public void filter(ContainerRequestContext containerRequestContext, ContainerResponseContext containerResponseContext) {
         Log.info("Executing CORS filter");
         //containerResponseContext.getHeaders().remove("Access-Control-Allow-Origin");
         containerResponseContext.getHeaders().add("Access-Control-Allow-Origin", "http://localhost:5173");
